a5e807191dcd1ba300d8ca9a995fea21467a57a6
AlejandroPuentes <puentesalejandro12@outlook.com>
2024-07-02 18:42:53 -0600

    Programa 19 - Conjuntos Practica

---
+++ ejercicio19.py
@@ -0,0 +1,9 @@
+d_fruits = {"apple", "banana", "orange", "mango"}
+
+mid_fuits = {"strawberry", "watermelon", "pineapple"}
+
+fruits_ultra = d_fruits | mid_fuits
+
+fruits_ultra.add("Grapes")
+
+print(fruits_ultra)
